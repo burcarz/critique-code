459b8cc321e8a7a26acf453a7f2c170cc3cf2a3d
Sloan <Sloan.boyce@gmail.com>
2022-01-12 14:59:59 -0500

    added all basic model boilerplate, and commented. Upvote and downvote still not working

--- models/Post.js
+++ models/Post.js
@@ -8,29 +8,29 @@ Post.init(
     {
         id: {
             type: DataTypes.INTEGER,
-            allowNull: false,
+            allowNull: false, // allow null is always going to false on any model, since this is the primary reference point
             primaryKey: true,
-            autoIncrement: true
+            autoIncrement: true 
         },
 
-        title: {
+        title: { // title of the post (will not contain code)
             type: DataTypes.STRING,
             allowNull: false
         },
 
-        body: {
+        body: { // body of the post (can contain code)
             type: DataTypes.STRING,
             allowNull: false
         },
 
-        post_url: {
+        post_url: { // post url, with validation
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
                 isUrl: true
             }
         },
-        user_id: {
+        user_id: { // user id with reference to user model and key
             type: DataTypes.INTEGER,
             references: {
                 model: 'user',
@@ -40,10 +40,11 @@ Post.init(
     },
 
     {
+        // boiler plate setup
         sequelize,
         freezeTableName: true,
         underscored: true,
-        modelName: 'post'
+        modelName: 'post' // model name will always be lowercase and be directly the same name as the model itself
     }
 )
 
