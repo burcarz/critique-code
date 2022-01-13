6a3fae39cf260c67e9500c2bc3745b7878ca4741
burcar <zacharyburcar@gmail.com>
2022-01-13 12:03:10 -0500

    login/signup forms added

---
+++ public/js/login.js
@@ -0,0 +1,52 @@
+// sign up form that POSTS new user data to /api/users
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector("#password-signup").value.trim();
+
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (response.ok) {
+            console.log('success');
+        } else {
+            console.log('failure to signup');
+        }
+    }
+}
+
+// login form posts user info to our login route
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (responses.ok) {
+            console.log('success');
+        } else {
+            console.log('failure to login')
+        }
+    }
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
