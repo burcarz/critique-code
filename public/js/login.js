d20e501f92e8f005f52d4a24207ac4bd198d7829
burcar <zacharyburcar@gmail.com>
2022-01-13 18:28:35 -0500

    working login/signup forms

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,24 @@
+// modal activation, threads to the signup/login handlers
+async function btnClickHandler(event) {
+    event.preventDefault();
+    document.getElementById('log-btn').addEventListener('submit', loginFormHandler);
+    document.getElementById('signup-btn').addEventListener('submit', signupFormHandler);
+
+    let modal = document.getElementById('signup-modal');
+    modal.style.display = "block";
+
+    document.getElementsByClassName('close')[0].addEventListener('click', function() {
+        modal.style.display = "none"
+    });
+
+    window.onclick = function(event) {
+        let modal = document.getElementById('signup-modal');
+        if (event.target == modal) {
+            modal.style.display = "none"
+        }
+    }
+    console.log('clicked!');
+}
 // sign up form that POSTS new user data to /api/users
 async function signupFormHandler(event) {
     event.preventDefault();
@@ -23,30 +44,28 @@ async function signupFormHandler(event) {
         }
     }
 }
-
 // login form posts user info to our login route
-// async function loginFormHandler(event) {
-//     event.preventDefault();
+async function loginFormHandler(event) {
+    event.preventDefault();
 
-//     const username = document.querySelector('#username-login').value.trim();
-//     const password = document.querySelector('#password-login').value.trim();
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
 
-//     if (username && password) {
-//         const response = await fetch('/api/users/login', {
-//             method: 'POST',
-//             body: JSON.stringify({
-//                 username,
-//                 password
-//             }),
-//             headers: { 'Content-Type': 'application/json' }
-//         });
-//         if (responses.ok) {
-//             console.log('success');
-//         } else {
-//             console.log('failure to login')
-//         }
-//     }
-// }
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (response.ok) {
+            console.log('success');
+        } else {
+            console.log('failure to login')
+        }
+    }
+}
 
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
-// document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document.getElementById('signup-btn').addEventListener('click', btnClickHandler);
