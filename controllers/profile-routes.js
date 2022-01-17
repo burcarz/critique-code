dd7b23117be42ebf732bd1f64789b97092e686a1
tgtiburon <tg.tiburon@gmail.com>
2022-01-16 21:14:20 -0500

    user profile and create post mainly working

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -2,63 +2,92 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
 
+
+// GET /dashboard/ gets all posts while logged in
 router.get('/', (req,res) => {
 
-    // GET /profile   Find all posts?  Not sure what we want on this page
-    Post.findAll({
-        // TODO: I am not sure what data you want for this page? Right now it 
-        // pulls your posts
-        where: {
-          // use the ID from the session
-          user_id: req.session.user_id
-        },
-        attributes: [
-          'id',
-          'title',
-          'post_body',
-          'vote_count',
-          'created_at'
-       
-        ],
-        include: [
-          {
-            model: Comment,
-            attributes: [
-              'id', 
-              'comment_body', 
-              'post_id', 
-              'user_id', 
-              'created_at'],
-            include: {
-              model: User,
-              attributes: ['username']
-            }
-          },
-          {
-            model: User,
-            attributes: ['username']
-          }
-        ]
-      })
-        .then(dbPostData => {
-    
-          // serialize data before passing to template
-          const posts = dbPostData.map(post => post.get({ plain: true }));
-
-          res.render('profile', { posts, loggedIn: true });
-  
-        })
-        .catch(err => {
-            // Server error
-          console.log(err);
-          res.status(500).json(err);
-        });
+  Post.findAll({
+      where: {
+        // use the ID from the session
+       // user_id: req.session.user_id
+       // TODO: going to have to hardcode until login is working right
+      // id: 1
+      user_id: 1
+      },
+      attributes: [
+        'id',
+        'title',
+        'post_body',
+        'vote_count',
+        'user_id',
+        'tag_genre',
+        'tag_language',
+        'created_at'
    
-
-
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            'id',
+            'comment_body',
+            'user_id',
+            'post_id', 
+            'created_at'
+              ],
+          include: {
+            model: User,
+            attributes: [
+              'id',
+              'username',
+              'email',
+              'password'  
+              
+            ]
+          }
+        },
+        {
+          model: User,
+          attributes: [
+            'id',
+            'username',
+            'email',
+            'password' 
+          ]
+        }
+      ]
+    })
+      .then(dbPostData => {
+         
+        // serialize data before passing to template
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+   
+        // Render dashboard with posts
+        res.render('profile', { posts, loggedIn: true });
+       console.log(posts)
+      
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
+
+
+// GET /signup Check to see if logged in, if not send to signup page
+router.get('/create-post', (req, res)=> {
+  // if they are logged in redirect to a homepage if one exists.
+  // if (req.session.loggedIn) {
+  //     res.redirect('/');
+  //     return;
+  // }
+  // no variables need to be passed so only the page name
+  res.render('create-post');
+});
+
 
 
 module.exports = router;
