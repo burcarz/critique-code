3d71c8e89c524a7817d11357402be0604b0d6d52
Sloan <Sloan.boyce@gmail.com>
2022-01-17 18:58:08 -0500

    added search funcionality

--- public/js/search-language.js
+++ public/js/search-language.js
@@ -1,20 +1,21 @@
 // retrieves post via tag_language
 
 async function getPostByLanguage(language) { // change this function tomorrow to work with the url's that Zach made in homeroutes
-    const response = await fetch('api/posts/tag/' + language, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+   if(window.location.href.indexOf('tag') > -1) {
+       const url = window.location.href;
+       const urlArr = url.split('/');
+       urlArr.pop();
+       urlArr.push(language);
+       const newUrl = urlArr.join('/')
 
+       console.log(newUrl);
+       window.location = newUrl;
+   } else {
+       const url = window.location.href;
+       const newUrl = url + 'tag/' + language;
 
-    if(response.ok) {
-        console.log('success');
-        console.log(response.json());
-    } else {
-        console.log('Failed to find tag with that language');
-    }
+       window.location = newUrl;
+   }
 }
 
 document.querySelector('.search-form').addEventListener('submit', (e) => {
