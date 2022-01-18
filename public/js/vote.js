6fae29b28d3c07548e12683a523f3fab3ab8ba0e
Sloan <Sloan.boyce@gmail.com>
2022-01-18 18:46:51 -0500

    bug fixing

--- public/js/vote.js
+++ public/js/vote.js
@@ -1,23 +1,2 @@
-const voteClickHandler = async function(event) {
-    event.preventDefault();
-    const postEl = event.target;
-    //const postId = postEl.querySelector('.info').querySelector('.id-val');
 
-    // console.log(postId);
-   /* const response = await fetch(`/api/posts/vote/${postId}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });*/
 
-    /*if(response.ok) {
-        console.log('success');
-        console.log(event.target);
-    } else {
-        console.log('vote failed to seed')
-    }*/
-    console.log(postEl);
-}
-
-document.querySelector('.up-vote-icon').addEventListener('click', voteClickHandler);
