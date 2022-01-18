1270949f7cf315c6d476ddfb0382dac34d658633
tgtiburon <tg.tiburon@gmail.com>
2022-01-17 19:27:38 -0500

    removed more debug stuff

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -18,9 +18,7 @@ router.get('/', (req,res) => {
       where: {
         // use the ID from the session
         user_id: req.session.user_id
-       // TODO: going to have to hardcode until login is working right
-      // id: 1
-     // user_id: 1
+    
       },
       attributes: [
         'id',
